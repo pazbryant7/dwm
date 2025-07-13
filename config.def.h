9e9f58137fa11f3451985391d3bff5706d69a93a
Bryant Paz <pazbryant7@outlook.com>
2025-07-12 20:05:56 -0600

    feat(patch): movestack

--- config.def.h
+++ config.def.h
@@ -76,47 +76,88 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
+/* Keys */
+
+#include "movestack.c"
 static const Key keys[] = {
-	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY|ShiftMask,             XK_f,      togglefullscr,  {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	{ MODKEY,                       XK_Right,  viewnext,       {0} },
-	{ MODKEY,                       XK_Left,   viewprev,       {0} },
-	{ MODKEY|ShiftMask,             XK_Right,  tagtonext,      {0} },
-	{ MODKEY|ShiftMask,             XK_Left,   tagtoprev,      {0} },
-	{ MODKEY,                       XK_F5,     xrdb,           {.v = NULL } },
-	TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+    /* modifier             key                 function        argument */
+
+    /* --- WM Control & Session --- */
+    { MODKEY,               XK_q,               killclient,     {0} },
+    { MODKEY|Mod1Mask,      XK_e,               quit,           {0} },
+
+    /* --- Application Launchers --- */
+    { MODKEY,               XK_t,               spawn,          {.v = termcmd } },
+
+    /* --- Application Custom Launchers --- */
+    { MODKEY,               XK_e,		            spawn,          SHCMD("st -e yazi") },
+    { MODKEY|ShiftMask,     XK_t,               spawn,          SHCMD("alacritty") },
+    { MODKEY,               XK_r,               spawn,          SHCMD("~/bin/wm/read") },
+    { MODKEY,               XK_v,               spawn,          SHCMD("~/bin/wm/watch") },
+    { MODKEY,               XK_d,               spawn,          SHCMD("rofi -show run") },
+    { MODKEY,               XK_a,               spawn,          SHCMD("~/bin/wm/passmenu") },
+    { MODKEY|ShiftMask,     XK_r,               spawn,          SHCMD("~/bin/wm/recording") },
+    { MODKEY,               XK_s,               spawn,          SHCMD("~/bin/wm/screenkey") },
+    { MODKEY,               XK_u,               spawn,          SHCMD("~/bin/wm/switch-sink") },
+    { MODKEY,               XK_c,               spawn,          SHCMD("~/bin/wm/rofi-greenclip") },
+    { MODKEY,               XK_b,               spawn,          SHCMD("~/bin/wm/set-brightness") },
+    { MODKEY,               XK_o,               spawn,          SHCMD("~/bin/wm/passmenu --otp") },
+    { MODKEY,               XK_space,		        spawn,          SHCMD("~/bin/wm/switch-keyboard") },
+    { MODKEY,               XK_m,               spawn,          SHCMD("st -t ncmpcpp -e ncmpcpp") },
+    { MODKEY,               XK_i,               spawn,          SHCMD("~/bin/wm/select-background") },
+    { MODKEY|ShiftMask,     XK_Return,          spawn,          SHCMD("brave --profile-directory='work'") },
+    { MODKEY,               XK_Return,          spawn,          SHCMD("brave --profile-directory='personal'") },
+
+    /* --- Window Management --- */
+    { MODKEY,		            XK_backslash,       setlayout,      {0} },
+    { MODKEY,               XK_n,               viewnext,       {0} },
+    { MODKEY|ShiftMask,     XK_w,               togglebar,      {0} },
+    { MODKEY,               XK_w,               togglefloating, {0} },
+	  { MODKEY,               XK_Tab,             zoom,           {0} },
+    { MODKEY,               XK_Escape,		      view,           {0} },
+    { MODKEY,               XK_f,               togglefullscr,  {0} },
+    { MODKEY,               XK_p,               viewprev,       {0} },
+
+    { MODKEY,               XK_F5,							xrdb,           {.v = NULL } },
+    { MODKEY,               XK_h,               focusstack,     {.i = -1 } },
+    { MODKEY,               XK_l,               focusstack,     {.i = +1 } },
+    { MODKEY|Mod1Mask,      XK_l,               setmfact,       {.f = +0.05} },
+    { MODKEY|Mod1Mask,      XK_h,               setmfact,       {.f = -0.05} },
+    { MODKEY|ShiftMask,     XK_l,               movestack,      {.i = +1 } },
+    { MODKEY|ShiftMask,     XK_h,               movestack,      {.i = -1 } },
+
+    /* --- Tags --- */
+    TAGKEYS(XK_bracketleft, 0)
+    TAGKEYS(XK_braceleft,   1)
+    TAGKEYS(XK_parenleft,   2)
+    TAGKEYS(XK_percent,     3)
+    TAGKEYS(XK_equal,       4)
+    TAGKEYS(XK_ampersand,   5)
+    TAGKEYS(XK_parenright,  6)
+    TAGKEYS(XK_braceright,  7)
+    TAGKEYS(XK_bracketright,8)
+    TAGKEYS(XK_exclam,      9)
+    TAGKEYS(XK_1,           0)
+    TAGKEYS(XK_2,           1)
+    TAGKEYS(XK_3,           2)
+    TAGKEYS(XK_4,           3)
+    TAGKEYS(XK_5,           4)
+    TAGKEYS(XK_6,           5)
+    TAGKEYS(XK_7,           6)
+    TAGKEYS(XK_8,           7)
+    TAGKEYS(XK_9,           8)
+    TAGKEYS(XK_0,           9)
+
+    /* --- Screenshots --- */
+    { ControlMask,XK_Print,   spawn, SHCMD("~/bin/wm/take-screenshots -m full -s save && notify-send 'Saved in disk'") },
+    { Mod1Mask,   XK_Print,   spawn, SHCMD("~/bin/wm/take-screenshots -m select -s save && notify-send 'Saved in disk'") },
+    { MODKEY,     XK_Print,   spawn, SHCMD("~/bin/wm/take-screenshots -m full -s clip && notify-send 'Saved in clipboard'") },
+    { 0,          XK_Print,   spawn, SHCMD("~/bin/wm/take-screenshots -m select -s clip && notify-send 'Saved in clipboard'") },
+
+    /* --- Hardware Keys --- */
+    { 0, XF86XK_AudioRaiseVolume, spawn, SHCMD("~/bin/wm/volume-dunst up") },
+    { 0, XF86XK_AudioLowerVolume, spawn, SHCMD("~/bin/wm/volume-dunst down") },
+    { 0, XF86XK_AudioMute,        spawn, SHCMD("~/bin/wm/volume-dunst mute") },
 };
 
 /* button definitions */
