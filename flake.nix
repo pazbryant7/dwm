8d80c4c61993015d207c6a7bdd696d4a0f3d32f0
Bryant Paz <pazbryant7@outlook.com>
2026-07-17 23:11:36 -0600

    feat(nix): adding a flake

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "My dwm build";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        packages.default = pkgs.dwm.overrideAttrs (old: {
+          src = ./.;
+        });
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            patch
+            clang-tools
+          ];
+        };
+        formatter = pkgs.nixfmt-tree;
+      }
+    );
+}
