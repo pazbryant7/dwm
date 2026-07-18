67606afbe6f86a1212caa2f55c7c09d856d84c24
Bryant Paz <pazbryant7@outlook.com>
2026-07-18 01:12:56 -0600

    feat(borderpx): increment to 2

--- config.def.h
+++ config.def.h
@@ -2,7 +2,7 @@
 #include <X11/XF86keysym.h>
 
 /* appearance */
-static const unsigned int borderpx = 1; /* border pixel of windows */
+static const unsigned int borderpx = 2; /* border pixel of windows */
 static const unsigned int snap = 32;    /* snap pixel */
 static const unsigned int systraypinning =
     0; /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor
