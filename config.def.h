68f273a4b89f935c0aebed533e88bb3e1b1614ad
Bryant Paz <pazbryant7@outlook.com>
2026-08-06 00:29:48 -0600

    feat(key): remove unused key

--- config.def.h
+++ config.def.h
@@ -138,7 +138,6 @@ static const Key keys[] = {
 
     /* --- UI & Bar --- */
     {MODKEY | ShiftMask, XK_w, togglebar, {0}},
-    {MODKEY, XK_F5, xrdb, {.v = NULL}},
 
     /* --- System Core & App Launchers --- */
     {MODKEY, XK_t, spawn, SHCMD("st")},
